9adfe1a2b768881284b7faaaf4712ca54117f271
yenlin <35764821+yenlin-chen@users.noreply.github.com>
2023-01-12 18:12:17 +0800

    ignore BiopythonDeprecatedWarning

--- experiments/exp-20230107-1.all_subgraphs.amd/train-amd.py
+++ experiments/exp-20230107-1.all_subgraphs.amd/train-amd.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import warnings
+from Bio import BiopythonDeprecationWarning
+warnings.filterwarnings("ignore", category=BiopythonDeprecationWarning)
+
 from sys import path as systemPath
 systemPath.append('../../')
 
