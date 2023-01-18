db674c5a95dee7a5bc0d85c6a5c9e7ff84f4b5b7
yenlin <35764821+yenlin-chen@users.noreply.github.com>
2023-01-18 17:18:15 +0800

    added some cache files for reference

---
+++ preprocessing/cache/summarize_mfgo.py
@@ -0,0 +1,24 @@
+import json
+from os import listdir
+from numpy import savetxt
+
+mfgo_dir = './mfgo/'
+keys = ['category', 'definition', 'identifier', 'name']
+
+all_pdbs = []
+all_mfgo = {}
+for json_file in [f for f in listdir(mfgo_dir) if '.json' in f]:
+    all_pdbs.append(json_file.replace('.json', ''))
+    with open(mfgo_dir+json_file, 'r') as f:
+        go_dict = json.load(f)
+
+    for code in go_dict:
+        if go_dict[code]['category'] == 'Molecular_function':
+            if code not in all_mfgo.keys():
+                all_mfgo[code] = {key: go_dict[code][key] for key in keys}
+
+with open('./non-exclusive_mfgo_list.json', 'w+') as f:
+    json.dump(all_mfgo, f,
+              indent=4, separators=(',', ': '), sort_keys=True)
+
+savetxt('./all_pdbs.txt', all_pdbs, fmt='%s')
