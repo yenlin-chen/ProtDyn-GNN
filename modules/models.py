cb4fce9f49caa791903788a3e3c3dffece6f07e9
yenlin <35764821+yenlin-chen@users.noreply.github.com>
2023-01-22 00:47:50 +0800

    added support for multi-GCN model (separate extractor for each graph dimension)

--- modules/models.py
+++ modules/models.py
@@ -12,8 +12,8 @@ from torchinfo import summary
 
 from torch_geometric.nn import (
     GCNConv,
-    GATConv,
-    SAGEConv,
+    # GATConv,
+    # SAGEConv,
     global_max_pool,
     LayerNorm
 )
@@ -24,7 +24,8 @@ class singleGCN(nn.Module):
                  dim_node_hidden,
                  dim_pers_embedding, dim_graph_embedding,
                  dropout_rate,
-                 n_graph_layers):
+                 n_graph_layers,
+                 gradCAM=False):
         '''Instantiate all components with trainable parameters'''
 
         self.all_args = locals()
@@ -35,13 +36,17 @@ class singleGCN(nn.Module):
         super().__init__()
 
         # the convolutional layers
-        self.conv_layers = nn.ModuleList([])
+        self.conv_block = nn.ModuleList([])
         for i in range(n_graph_layers):
             dim_input = dim_node_feat if i == 0 else dim_node_hidden
 
-            conv = GCNConv(dim_input, dim_node_hidden,
-                           aggr='add', improved=False, add_self_loops=True)
-            self.conv_layers.append(conv)
+            conv = GCNConv(dim_input, dim_node_hidden)
+            # default arguments to GCNConv (and MessagePassing)
+            # aggr='add', improved=False, add_self_loops=True
+            self.conv_block.append(conv)
+
+        if gradCAM:
+            pass
 
         # linear layers to process convolutional features
         self.graph_block = nn.Sequential(
@@ -75,7 +80,7 @@ class singleGCN(nn.Module):
 
         # pipe features from each layer of convolution into the fc layer
         jk_connection = []
-        for idx, conv in enumerate(self.conv_layers):
+        for layer_idx, conv in enumerate(self.conv_block):
             x = conv(x, data.edge_index)
             x = F.relu(x)
             jk_connection.append(x)
@@ -110,10 +115,85 @@ class singleGCN(nn.Module):
         sys.stdout = sys.__stdout__
 
 class multiGCN(nn.Module):
-    def __init__(self):
-        pass
-    def forward(self):
-        pass
+    def __init__(self, n_dims,
+                 dim_node_feat, dim_pers_feat, dim_out,
+                 dim_node_hidden,
+                 dim_pers_embedding, dim_graph_embedding,
+                 dropout_rate,
+                 n_graph_layers,
+                 gradCAM=False):
+        '''Instantiate all components with trainable parameters'''
+
+        self.all_args = locals()
+        del self.all_args['self'], self.all_args['__class__']
+
+        self.n_dims = n_dims
+        self.dim_node_hidden = dim_node_hidden
+
+        super().__init__()
+
+        # the convolutional layers
+        self.conv_block_list = nn.ModuleList([])
+        for dim_idx in range(n_dims):
+            conv_block = nn.ModuleList([])
+            for layer_idx in range(n_graph_layers):
+                dim_input = dim_node_feat if layer_idx == 0 else dim_node_hidden
+                conv = GCNConv(dim_input, dim_node_hidden)
+                conv_block.append(conv)
+            self.conv_block_list.append(conv_block)
+
+        # linear layers for processing convoluted vectors
+        self.graph_block = nn.Sequential(
+            nn.Linear(n_dims * n_graph_layers * dim_node_hidden,
+                      dim_graph_embedding),
+            nn.LayerNorm(dim_graph_embedding),
+            nn.Dropout(p=dropout_rate),
+            nn.ReLU()
+        )
+
+        # linear layers to process the persistence diagram
+        self.pi_block = nn.Sequential(
+            nn.Linear(dim_pers_feat, dim_pers_embedding),
+            nn.LayerNorm(dim_pers_embedding),
+            nn.Dropout(p=dropout_rate),
+            nn.ReLU()
+        ) if dim_pers_embedding else None
+
+        # final layers to combine everything above
+        dim_total_embedding = dim_graph_embedding + dim_pers_embedding
+        self.fc_block = nn.Sequential(
+            nn.Linear(dim_total_embedding, dim_out),
+            nn.LayerNorm(dim_out),
+            nn.Dropout(p=dropout_rate)
+        )
+
+    def forward(self, data):
+        '''Make connects between the components to complete the model'''
+
+        # pipe features from each layer of convolution into the fc layer
+        jk_connection = []
+        for dim_idx in range(self.n_dims):
+            x = data.x
+            dim_slice = torch.argwhere(data.edge_type[:,dim_idx]==1).squeeze()
+            dim_edge_index = data.edge_index[:, dim_slice]
+            for layer_idx, conv in enumerate(self.conv_block_list[dim_idx]):
+                x = conv(x, dim_edge_index)
+                x = F.relu(x)
+                jk_connection.append(x)
+
+        jk_connection = torch.cat(jk_connection, dim=1)
+        x = global_max_pool(jk_connection, data.batch)
+
+        if self.pi_block: # graph & persistence embedding (concatenated)
+            x = torch.cat((self.graph_block(x), self.pi_block(data.pi.float())),
+                          dim=1)
+        else: # graph embedding only
+            x = self.graph_block(x)
+
+        x = self.fc_block(x)
+
+        return x
+
     def save_args(self, save_dir):
         with open(path.join(save_dir, 'model-args.json'),
                   'w+') as f_out:
